6156b8be1bdb58589ae45ba0e61ac154cb50b17d
MrJuanV <139188459+MrJuanV@users.noreply.github.com>
2023-07-14 16:34:42 -0400

    Add files via upload

---
+++ tarea.js
@@ -0,0 +1,18 @@
+//Crea una variable numero y asígnale un valor de tipo number.
+//var a = 1;
+
+//var a =123
+
+//console.log(a)
+//Crea una variable texto y asígnale un valor de tipo string.
+//let mivariable="este es un texto"
+
+//
+function Ispar (numero){
+    if(numero % 2 == 0){
+        return true
+    }else {
+        return false
+    }
+    }
+    console.log(Ispar(8))
